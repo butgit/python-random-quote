c4078ed9ddad4a9d325da593e29d14456105b7e4
butgit <schooler@daum.net>
2019-08-24 16:52:23 +0900

    get random quote

--- get-quote.py
+++ get-quote.py
@@ -1,9 +1,14 @@
+import random
+
 def primary():
   f = open("quotes.txt", encoding='utf-8')
   quotes = f.readlines()
   f.close()
 
-  print(quotes[0])
+  last = len(quotes) - 1
+  rnd = random.randint(0, last)
+
+  print(quotes[rnd])
 
 if __name__== "__main__":
   primary()
